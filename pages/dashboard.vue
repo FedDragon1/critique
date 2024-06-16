3516249b941b6aa5384fa1c06dd3fa5a6bd49ba2
fedel <fedelewu@outlook.com>
2024-06-16 13:01:54 -0700

    supabase test

---
+++ pages/dashboard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware: 'auth'
+})
+
+const user = useSupabaseUser();
+</script>
+
+<template>
+{{user}}
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+
+
+const signInWithGoogle = async() => {
+  const ret = await supabase.auth.signInWithOAuth({
+    provider: "google"
+  })
+  console.log(ret)
+}
+const signInWithGithub = async() => {
+  const ret = await supabase.auth.signInWithOAuth({
+    provider: "github"
+  })
+  console.log(ret)
+}
+
+</script>
+
+<template>
+<button type="button" @click="signInWithGoogle">Login with Google</button>
+<button type="button" @click="signInWithGithub">Login with Github</button>
+</template>
+
+<style scoped>
+
+</style>
